04e8bf69351b3a59a5598aa4cef7bd2e48e671f3
Vishnu Anand <vishnuanand2000@gmail.com>
2022-06-22 21:34:49 +0530

    Created repo and extended JpaRepository with EmployeeEntity

---
+++ employee-system-api/src/main/java/com/vishnu/employee/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.vishnu.employee.repository;
+
+import com.vishnu.employee.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
