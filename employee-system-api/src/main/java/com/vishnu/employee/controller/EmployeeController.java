53849741f113129c960a71b4e1be7122cbee8db0
Vishnu Anand <vishnuanand2000@gmail.com>
2022-06-22 21:49:19 +0530

    Added create Employee method with Post mapping

--- employee-system-api/src/main/java/com/vishnu/employee/controller/EmployeeController.java
+++ employee-system-api/src/main/java/com/vishnu/employee/controller/EmployeeController.java
@@ -1,7 +1,10 @@
 package com.vishnu.employee.controller;
 
+import com.vishnu.employee.model.Employee;
 import com.vishnu.employee.service.EmployeeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +19,10 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
+    // POST Method to handle post request to save employee details
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeService.createEmployee(employee);
+    }
 
 }
