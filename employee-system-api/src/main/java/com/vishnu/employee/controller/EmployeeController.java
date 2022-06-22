67199f39bb2efcfb5de64862c4504f1ada2ac820
Vishnu Anand <vishnuanand2000@gmail.com>
2022-06-22 22:20:47 +0530

    Added cross origin permission

--- employee-system-api/src/main/java/com/vishnu/employee/controller/EmployeeController.java
+++ employee-system-api/src/main/java/com/vishnu/employee/controller/EmployeeController.java
@@ -3,11 +3,9 @@ package com.vishnu.employee.controller;
 import com.vishnu.employee.model.Employee;
 import com.vishnu.employee.service.EmployeeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000") // To allow CORS
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
