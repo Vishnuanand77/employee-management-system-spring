b2b6717e13f27f9004718e5d37e6c92e0203ee96
Vishnu Anand <vishnuanand2000@gmail.com>
2022-06-22 22:30:45 +0530

    Added fetchAllEmployees() method

--- employee-system-api/src/main/java/com/vishnu/employee/service/EmployeeService.java
+++ employee-system-api/src/main/java/com/vishnu/employee/service/EmployeeService.java
@@ -2,6 +2,10 @@ package com.vishnu.employee.service;
 
 import com.vishnu.employee.model.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
     Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
 }
--- employee-system-api/src/main/java/com/vishnu/employee/service/EmployeeServiceImpl.java
+++ employee-system-api/src/main/java/com/vishnu/employee/service/EmployeeServiceImpl.java
@@ -6,6 +6,9 @@ import com.vishnu.employee.repository.EmployeeRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
 
@@ -25,5 +28,22 @@ public class EmployeeServiceImpl implements EmployeeService{
         employeeRepository.save(employeeEntity);
 
         return employee;
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+
+        List<EmployeeEntity> employeeEntities = employeeRepository.findAll();
+
+        List<Employee> employees = employeeEntities
+                .stream()
+                .map(emp -> new Employee(
+                        emp.getId(),
+                        emp.getFirstName(),
+                        emp.getLastName(),
+                        emp.getEmailId()))
+                .toList();
+
+        return employees;
     }
 }
