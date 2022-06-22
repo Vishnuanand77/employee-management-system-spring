23521115ada98ec950c34fb6bbf38da8617550ab
Vishnu Anand <vishnuanand2000@gmail.com>
2022-06-22 21:31:55 +0530

    Created controller class

--- employee-system-api/src/main/java/com/vishnu/employee/controller/EmployeeController.java
+++ employee-system-api/src/main/java/com/vishnu/employee/controller/EmployeeController.java
@@ -1,9 +1,20 @@
 package com.vishnu.employee.controller;
 
+import com.vishnu.employee.service.EmployeeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
+
+    @Autowired
+    private final EmployeeServiceImpl employeeService;
+
+    public EmployeeController(EmployeeServiceImpl employeeService) {
+        this.employeeService = employeeService;
+    }
+
+
 }
