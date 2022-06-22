a216213e14fa85be9e41b6a9d1a37d2853cda52a
Vishnu Anand <vishnuanand2000@gmail.com>
2022-06-22 21:49:31 +0530

    Added create Employee methods

--- employee-system-api/src/main/java/com/vishnu/employee/service/EmployeeService.java
+++ employee-system-api/src/main/java/com/vishnu/employee/service/EmployeeService.java
@@ -1,4 +1,7 @@
 package com.vishnu.employee.service;
 
+import com.vishnu.employee.model.Employee;
+
 public interface EmployeeService {
+    Employee createEmployee(Employee employee);
 }
--- employee-system-api/src/main/java/com/vishnu/employee/service/EmployeeServiceImpl.java
+++ employee-system-api/src/main/java/com/vishnu/employee/service/EmployeeServiceImpl.java
@@ -1,7 +1,29 @@
 package com.vishnu.employee.service;
 
+import com.vishnu.employee.entity.EmployeeEntity;
+import com.vishnu.employee.model.Employee;
+import com.vishnu.employee.repository.EmployeeRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
+
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee createEmployee(Employee employee) {
+        EmployeeEntity employeeEntity = new EmployeeEntity();
+
+        // Copies all properties of employee to employeeEntity
+        BeanUtils.copyProperties(employee, employeeEntity);
+        // Saves the data
+        employeeRepository.save(employeeEntity);
+
+        return employee;
+    }
 }
