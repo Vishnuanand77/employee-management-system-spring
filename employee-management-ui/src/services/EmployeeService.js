4655cb2489d27901b347950019cd8ee03a152b48
Vishnu Anand <vishnuanand2000@gmail.com>
2022-06-23 12:34:37 +0530

    Added getEmployees() method

--- employee-management-ui/src/services/EmployeeService.js
+++ employee-management-ui/src/services/EmployeeService.js
@@ -10,6 +10,11 @@ class EmployeeService {
         return axios.post(EMPLOYEE_API_BASE_URL, employee);
     }
 
+    // Method to retrieve list of all employees
+    getEmployees() {
+        return axios.get(EMPLOYEE_API_BASE_URL);
+    }
+
 }
 
 export default new EmployeeService();
